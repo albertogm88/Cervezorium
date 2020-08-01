4b8d8187b3a442320cfca9d0f605b34c28f22beb
Alberto Garcia Mendez <alberto@MacBook-Pro-de-Alberto.local>
2020-08-01 12:09:30 +0200

    Commit inicial

---
+++ src/main/java/com/www/cervezorium/CervezoriumApplication.java
@@ -0,0 +1,13 @@
+package com.www.cervezorium;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CervezoriumApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CervezoriumApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/www/cervezorium/CervezoriumApplicationTests.java
@@ -0,0 +1,13 @@
+package com.www.cervezorium;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CervezoriumApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
